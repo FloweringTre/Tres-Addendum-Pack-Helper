cd73608633e7d2e9ad285e053d87a257406d4c37
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-12-29 00:46:34 -0700

    update coat scene
    - match pink color palette
    - remove unneeded information
    - add in needed information
    - update scripts to match the change in information

--- scripts/coat_gui.gd
+++ scripts/coat_gui.gd
@@ -1,28 +1,24 @@
 extends Control
 
-var breedable : bool = true
-var cyclable : bool = true
 var has_wings : bool = false
+var breed : String = "irish_draught"
+var fancy_breed: String = "Irish Draught - SWEM Base Model"
 
-var basecolors : Array
-var black_coat : bool = false
-var gray_coat : bool = false
-var white_coat : bool = false
-var creamy_coat : bool = false
-var brown_coat : bool = false
-var dark_brown_coat : bool = false
-var chestnut_coat : bool = false
-
-var artist : bool = false
 var coat_name : bool = false
 
 var file_name : String
 var path : String
 
 var image_coat : Image
+var image_wing : Image
 var text_coat : bool = false
+var text_wing : bool = false
 var coat_save_path : String
 var coat_source : String
+var wing_save_path : String
+var wing_source : String
+
+var dialog_opened : String 
 
 signal new_coat_saved
 
@@ -39,48 +35,28 @@ func _ready() -> void:
 	$popUPexit.confirm.connect(on_popup_exit_confirmed)
 	$helpscreen.visible = true
 	$FileDialog.current_dir = GlobalScripts.directory_root
-	if GlobalScripts.artist != "":
-		%artistText.text = GlobalScripts.artist
-		artist = true
+	$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer2/HBoxContainer3.visible = has_wings
 	ready_to_save()
 
 func disable_interaction () -> void:
 	%confirmButton.set_disabled()
 	%backButton.set_disabled()
-	%artistText.editable = false
-	%inspoText.editable = false
 	%coatText.editable = false
 	%renderButton.set_disabled()
 	%nameCheck.set_disabled()
-	%lapisCheckBox.disabled = true
-	%breedingCheckBox.disabled = true
+	%wingButton.set_disabled()
+	%breedoptions.disabled = true
 	%wingsCheckBox.disabled = true
-	%blackCheckBox.disabled = true
-	%grayCheckBox2.disabled = true
-	%whiteCheckBox3.disabled = true
-	%creamyCheckBox4.disabled = true
-	%brownCheckBox5.disabled = true
-	%d_brownCheckBox6.disabled = true
-	%chestnutCheckBox7.disabled = true
 
 func enable_interaction () -> void:
 	%confirmButton.reenable_button()
 	%backButton.reenable_button()
-	%artistText.editable = true
-	%inspoText.editable = true
 	%coatText.editable = true
 	%renderButton.reenable_button()
 	%nameCheck.reenable_button()
-	%lapisCheckBox.disabled = false
-	%breedingCheckBox.disabled = false
+	%wingButton.reenable_button()
+	%breedoptions.disabled = false
 	%wingsCheckBox.disabled = false
-	%blackCheckBox.disabled = false
-	%grayCheckBox2.disabled = false
-	%whiteCheckBox3.disabled = false
-	%creamyCheckBox4.disabled = false
-	%brownCheckBox5.disabled = false
-	%d_brownCheckBox6.disabled = false
-	%chestnutCheckBox7.disabled = false
 
 func on_error() -> void:
 	$popUPload.stop_loading()
@@ -93,20 +69,14 @@ func on_error_continue() -> void:
 ########### BUTTON LOGGING ################
 
 func _on_back_button_pressed() -> void:
-	if %artistText.text != "" or %inspoText.text != "" or %coatText.text != "" or text_coat:
+	if %coatText.text != "" or text_coat:
 		are_you_sure()
 	else:
+		TransitionFade.transition()
+		await TransitionFade.transition_finished
 		get_tree().change_scene_to_file("res://scene/startingGUI.tscn")
 
-func _on_artist_text_text_changed(new_text: String) -> void:
-	if %artistText.text != "":
-		artist = true
-		ready_to_save()
-	else:
-		artist = false
-		ready_to_save()
-
-func _on_coat_text_text_changed(new_text: String) -> void:
+func _on_coat_text_text_changed(_new_text: String) -> void:
 	$checkPath.awaiting_check()
 	coat_name = false
 	%confirmButton.set_disabled()
@@ -123,87 +93,16 @@ func on_name_check() -> void:
 		coat_name = true
 		ready_to_save()
 
-func _on_breeding_check_box_pressed() -> void:
-	if breedable:
-		breedable = false
-		%breedingLabel.text = "No"
-	else:
-		breedable = true
-		%breedingLabel.text = "Yes"
-
-func _on_lapis_check_box_pressed() -> void:
-	if cyclable:
-		cyclable = false
-		%lapisLabel.text = "No"
-	else:
-		cyclable = true
-		%lapisLabel.text = "Yes"
-
 func _on_wings_check_box_pressed() -> void:
 	if has_wings:
 		has_wings = false
 	else:
 		has_wings = true
 	%wingsLabel.text = "Yes" if has_wings else "No"
-
-func _on_black_check_box_pressed() -> void:
-	if black_coat:
-		black_coat = false
-		basecolors.erase("black")
-	else:
-		black_coat = true
-		basecolors.append("black")
-
-func _on_gray_check_box_2_pressed() -> void:
-	if gray_coat:
-		gray_coat = false
-		basecolors.erase("gray")
-	else:
-		gray_coat = true
-		basecolors.append("gray")
-
-func _on_white_check_box_3_pressed() -> void:
-	if white_coat:
-		white_coat = false
-		basecolors.erase("white")
-	else:
-		white_coat = true
-		basecolors.append("white")
-
-func _on_creamy_check_box_4_pressed() -> void:
-	if creamy_coat:
-		creamy_coat = false
-		basecolors.erase("creamy")
-	else:
-		creamy_coat = true
-		basecolors.append("creamy")
-
-func _on_brown_check_box_5_pressed() -> void:
-	if brown_coat:
-		brown_coat = false
-		basecolors.erase("brown")
-	else:
-		brown_coat = true
-		basecolors.append("brown")
-
-func _on_d_brown_check_box_6_pressed() -> void:
-	if dark_brown_coat:
-		dark_brown_coat = false
-		basecolors.erase("dark_brown")
-	else:
-		dark_brown_coat = true
-		basecolors.append("dark_brown")
-
-func _on_chestnut_check_box_7_pressed() -> void:
-	if chestnut_coat:
-		chestnut_coat = false
-		basecolors.erase("chestnut")
-	else:
-		chestnut_coat = true
-		basecolors.append("chestnut")
+	$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer2/HBoxContainer3.visible = has_wings
 
 func ready_to_save() -> void:
-	if artist && coat_name:
+	if coat_name:
 		%confirmButton.reenable_button()
 	else:
 		%confirmButton.set_disabled()
@@ -211,12 +110,16 @@ func ready_to_save() -> void:
 #################### SAVING PROCESS ######################
 
 func _on_confirm_button_pressed() -> void:
+	if (breed != "irish_draught") and (breed != "pegasus"):
+		has_wings = false
+		print(breed, " -  wing status - ", has_wings)
+	
 	$popUPload.loading("Checking for duplicates")
 	disable_interaction()
-	GlobalScripts.setup_coats()
+	GlobalScripts.make_coat_folder(breed)
 	on_name_check()
-	file_name = %coatText.text + ".json"
-	path = GlobalScripts.join_paths(GlobalScripts.jsons_root, "coats")
+	file_name = %coatText.text + ".png"
+	path = GlobalScripts.join_paths(GlobalScripts.textures_root, breed)
 	path = GlobalScripts.join_paths(path, file_name)
 	
 	if GlobalScripts.check_file_exists(path):
@@ -227,43 +130,23 @@ func _on_confirm_button_pressed() -> void:
 
 func save_coat() -> void:
 	$popUPload.loading("Saving coat")
-	if %inspoText.text == "":
-		%inspoText.text = "N/a"
-	var save_path = GlobalScripts.join_paths(GlobalScripts.textures_root, "coats/legacy")
-	var file = FileAccess.open(path, FileAccess.WRITE)
-	if file:
-		var models = {
-			"legacy" = "legacy/" + %coatText.text + ".png"
-		}
-		var part_1 = {
-			"credits" = %artistText.text,
-			"inspiration" = %inspoText.text,
-			"models" = models,
-			"lapis_cyclable" = cyclable,
-			"obtainable_by_breeding" = breedable,
-			"wings" = has_wings,
-			"base_colors" = basecolors
-		}
-		
-		var string_1 = JSON.stringify(part_1, "   ")
-		
-		file.store_string(string_1)
-		file.close()
-		GlobalScripts.report("Saved the new coat, " + %coatText.text + ", to " + path)
-		
-		if text_coat:
-			coat_save_path = save_path + "/" + %coatText.text + ".png"
-			image_coat.save_png(coat_save_path)
-			GlobalScripts.report("Saved user selected image: " + coat_source + "  to the file location: " + coat_save_path)
-		else:
-			GlobalScripts.instructions_coat(%coatText.text, GlobalScripts.join_paths(GlobalScripts.textures_root, "coats/legacy") )
-		$popUPload.stop_loading()
-		new_coat_saved.emit()
-	
+	var save_path = GlobalScripts.join_paths(GlobalScripts.textures_root, breed)
+	if text_coat:
+		coat_save_path = save_path + "/" + %coatText.text + ".png"
+		image_coat.save_png(coat_save_path)
+		GlobalScripts.report("Saved user selected image: " + coat_source + "  to the file location: " + coat_save_path)
 	else:
-		$popUPload.stop_loading()
-		ErrorManager.error_print("I couldn't save the new coat. The ./json/coat/ folder wouldn't open. Check to see if it exists.")
-		GlobalScripts.report("Failed to save the new coat, '" + %coatText.text + "' to " + path)
+		GlobalScripts.instructions_coat(%coatText.text, save_path, has_wings)
+	
+	if has_wings:
+		if text_wing:
+			wing_save_path = save_path + "/" + %coatText.text + "_wing.png"
+			image_coat.save_png(wing_save_path)
+			GlobalScripts.report("Saved user selected image: " + wing_source + "  to the file location: " + wing_save_path)
+	
+	GlobalScripts.summon_log(%coatText.text, fancy_breed)
+	$popUPload.stop_loading()
+	new_coat_saved.emit()
 
 func coat_exists() -> void:
 	var title = "This coat already exists!"
@@ -273,7 +156,7 @@ func coat_exists() -> void:
 	$popUP2_Dupe.pop_yesNo(title, message, no_label, yes_label)
 	disable_interaction()
 
-############### POP UP HANDLING ##########################
+#region ############### POP UP HANDLING ##########################
 
 func on_popup_dupe_back() -> void:
 	enable_interaction()
@@ -290,6 +173,8 @@ func on_new_coat_saved() -> void:
 	disable_interaction()
 
 func on_popup_saved_back() -> void:
+	TransitionFade.transition()
+	await TransitionFade.transition_finished
 	get_tree().change_scene_to_file("res://scene/startingGUI.tscn")
 
 func on_popup_saved_confirmed() -> void:
@@ -308,18 +193,103 @@ func on_popup_exit_back() -> void:
 	enable_interaction()
 
 func on_popup_exit_confirmed() -> void:
+	TransitionFade.transition()
+	await TransitionFade.transition_finished
 	get_tree().change_scene_to_file("res://scene/startingGUI.tscn")
+#endregion
 
+#region Image values
 func _on_file_dialog_file_selected(selected_path: String) -> void:
-	text_coat = true
-	coat_source = path
-	image_coat = Image.load_from_file(selected_path)
-	%renderButton.button_label.text = "Coat"
+	if dialog_opened == "coat":
+		text_coat = true
+		coat_source = path
+		image_coat = Image.load_from_file(selected_path)
+		%renderButton.button_label.text = "Coat"
 
-	var image_file_name = selected_path.split("\\")
-	image_file_name = image_file_name[-1]
-	%renderLineEdit.text = " " + image_file_name
+		var image_file_name = selected_path.split("\\")
+		image_file_name = image_file_name[-1]
+		%renderLineEdit.text = " " + image_file_name
+	if dialog_opened == "wing":
+		text_wing = true
+		wing_source = path
+		image_wing = Image.load_from_file(selected_path)
+		%wingButton.button_label.text = "Wing"
+
+		var image_file_name = selected_path.split("\\")
+		image_file_name = image_file_name[-1]
+		%wingLineEdit.text = " " + image_file_name
 
 func _on_render_button_button_pressed() -> void:
+	dialog_opened = "coat"
 	$FileDialog.title = "Select the Coat Texture"
 	$FileDialog.visible = true
+
+func _on_wing_button_button_pressed() -> void:
+	dialog_opened = "wing"
+	$FileDialog.title = "Select the Wing Texture"
+	$FileDialog.visible = true
+#endregion
+
+func _on_breedoptions_item_selected(index: int) -> void:
+	$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer/wings.visible = false
+	$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer2/HBoxContainer3.visible = false
+	match index:
+		0:
+			breed = "american_quarter_horse"
+			fancy_breed = "American Quarter Horse"
+		1:
+			breed = "arabian"
+			fancy_breed = "Arabian"
+		2:
+			breed = "breton"
+			fancy_breed = "Breton"
+		3:
+			breed = "donkey"
+			fancy_breed = "Donkey"
+		4:
+			breed = "fjord"
+			fancy_breed = "Fjord"
+		5:
+			breed = "friesian"
+			fancy_breed = "Friesian"
+		6:
+			breed = "irish_draught"
+			fancy_breed = "Irish Draught - SWEM Base Model"
+			$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer/wings.visible = true
+			$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer2/HBoxContainer3.visible = has_wings
+		7:
+			breed = "kladruber"
+			fancy_breed = "Kladruber"
+		8:
+			breed = "knabstrupper"
+			fancy_breed = "Knabstrupper"
+		9:
+			breed = "marwari"
+			fancy_breed = "Marwari"
+		10:
+			breed = "mule"
+			fancy_breed = "Mule"
+		11:
+			breed = "mustang"
+			fancy_breed = "Mustang"
+		12:
+			breed = "pegasus"
+			fancy_breed = "Pegasus"
+			$NinePatchRect/VBoxContainer/HBoxContainer/VBoxContainer/wings.visible = true
+			%wingsCheckBox.button_pressed = true
+			_on_wings_check_box_pressed()
+		13:
+			breed = "shire"
+			fancy_breed = "Shire"
+		14:
+			breed = "thoroughbred"
+			fancy_breed = "Thoroughbred"
+		15:
+			breed = "turkoman"
+			fancy_breed = "Turkoman"
+		16:
+			breed = "warmblood"
+			fancy_breed = "Warmblood"
+		17:
+			breed = "steed_foal"
+			fancy_breed = "Steed Foal Coat"
